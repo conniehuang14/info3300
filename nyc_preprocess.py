c84e24f6b9798af23cc42951ec96c7b970de4433
Hunter Greene <hjg43@cornell.edu>
2021-05-01 16:41:26 -0400

    Altered rat dataset, made counts

--- nyc_preprocess.py
+++ nyc_preprocess.py
@@ -8,19 +8,26 @@ rat_df = pd.read_csv("Rat_Sightings.csv")
 # Create column for year of sighting
 rat_df['Year'] = [datetime.strptime(ts.replace(" AM", "").replace(" PM", ""), "%m/%d/%Y %H:%M:%S").strftime("%Y") for ts in rat_df['Created Date']]
 
-# Generate separate output df to export, select only necessary columns
-output_df = pd.DataFrame(data={'year' : rat_df['Year'], 'borough': [x.lower() for x in rat_df['Borough']], 'zip':rat_df['Incident Zip'], 'latitude':rat_df['Latitude'], 'longitude':rat_df['Longitude']})
-output_df = output_df.rename_axis("id")
+# Count rats by zip code and year, append to output dataframe
+rat_output_df = pd.DataFrame()
+years = [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017]
+for year in years:
+    rat_groups = rat_df.loc[rat_df['Year'] == str(year)].loc[rat_df['Incident Zip'] > 10000].groupby('Incident Zip')['Unique Key'].nunique().to_frame()
+    rat_groups['year'] = year
+    rat_groups['borough'] = rat_df.loc[rat_df['Year'] == str(year)].iloc[0]['Borough'].lower()
+    rat_output_df = rat_output_df.append(rat_groups)
 
-# Export as csv
-output_df.to_csv('rat_data.csv')
+# Cast zip codes to int and export as csv
+rat_output_df = rat_output_df.reindex(rat_output_df.index.astype(int))
+rat_output_df = rat_output_df.rename(columns={'Unique Key':'count'}).rename_axis("zip")
+rat_output_df.to_csv('rat_data.csv')
 
 # Create sale property output csv, append filtered data over loop
 sale_output_df = pd.DataFrame()
 
 # NYC Property Sales source: https://www.kaggle.com/johnshuford/new-york-city-property-sales
 # Construct read_csv statement for looping over multiple csvs by year and borough
-years = [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017]
+
 boroughs = ['Bronx', 'Brooklyn', 'Manhattan', 'Queens', 'StatenIsland']
 for year in years:
     for borough in boroughs:
